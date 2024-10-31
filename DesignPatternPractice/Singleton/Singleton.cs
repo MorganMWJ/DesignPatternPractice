0152beb040eaab56dd813ecc62584c94e78297d5
Morgan Jones <morganmwj@gmail.com>
2024-10-31 13:08:46 +0000

    Singleton

---
+++ DesignPatternPractice/Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatternPractice.Singleton;
+
+/// <summary>
+/// Example Singleton for reference.
+/// This is not needed use static objects or .net DI singleton scope.
+/// </summary>
+internal class Singleton
+{
+    private static Singleton? Instance;
+
+    private Singleton()
+    {        
+    }
+
+    public Singleton GetInstance()
+    {
+        Instance ??= new Singleton();
+
+        return Instance;
+    }
+}
